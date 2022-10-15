a8141ba35e67123d4229ac9ab426a3896b624f31
wolfgang <wolfgang.d24@gmail.com>
2022-10-15 18:57:04 +0200

    slices and maps

--- projectone.go
+++ projectone.go
@@ -4,4 +4,22 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, world!")
+
+	var arr []string
+
+	arr = make([]string, 3)
+	arr = append(arr, "Hello")
+	arr = append(arr, "Test")
+	arr = append(arr, "Good")
+
+	for _, value := range arr {
+		fmt.Println(value)
+	}
+
+	userlist := map[string]int{"Werner": 10, "Heiner": 20}
+
+	for index, user := range userlist {
+		fmt.Println(index)
+		fmt.Println(user)
+	}
 }
